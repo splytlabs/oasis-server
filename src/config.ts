973f44bdf9ab1d0d525a4b861cce4420d826841d
yun-sangho <contact@yunsangho.com>
2022-09-30 12:11:44 +0900

    feat: config 관리 수정

---
+++ src/config.ts
@@ -0,0 +1,63 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: path.join(__dirname, '../.env.production') });
+} else if (process.env.NODE_ENV === 'develop') {
+  dotenv.config({ path: path.join(__dirname, '../.env') });
+} else {
+  throw new Error('process.env.NODE_ENV를 설정하지 않았습니다!');
+}
+
+type CastType = {
+  number: number;
+  string: string;
+};
+
+function number(value: string): number {
+  const result = Number(value);
+  if (!Number.isNaN(result)) {
+    return result;
+  } else {
+    throw new Error(`Cannot cast ${value} to number`);
+  }
+}
+
+function string(value: string) {
+  return value;
+}
+
+const typeConverter = { number, string };
+
+function cast<T extends keyof CastType>(
+  key: string,
+  type: T,
+  defaultValue: CastType[T],
+): CastType[T] {
+  const value = process.env[key];
+  if (value !== undefined) {
+    try {
+      return typeConverter[type](value) as CastType[T];
+    } catch (e) {
+      throw new Error(`process.env.${key}에 적절한 값을 설정하지 않았습니다`);
+    }
+  }
+  if (defaultValue !== undefined) {
+    return defaultValue;
+  }
+  throw new Error(`process.env.${key}에 할당할 값이 없습니다`);
+}
+
+export const config = {
+  dbHost: cast('DB_HOST', 'string', 'localhost'),
+  dbPort: cast('DB_PORT', 'number', 6519),
+  dbUser: cast('DB_NAME', 'string', 'admin1234'),
+  dbPassword: cast('DB_PASSWORD', 'string', 'admin'),
+  databaseName: cast('DB_NAME', 'string', 'postgres'),
+  dbPoolCount: cast('DB_POOL_COUNT', 'number', 5),
+  klayRpcUrl: cast(
+    'KLAYTN_RPC_URL',
+    'string',
+    'https://api.baobab.klaytn.net:8651',
+  ),
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import 'reflect-metadata';
-import 'dotenv/config';
 
 import { JsonRpcProvider } from '@ethersproject/providers';
 import { Contract } from 'ethers';
@@ -7,22 +6,23 @@ import { readFileSync } from 'fs';
 import { Pool } from 'pg';
 import { DbService } from './services/db';
 import { QueryBuilder } from './services/query';
+import { config } from './config';
 
 const contractRawData = readFileSync('./src/market.json').toString();
 
 const abi = JSON.parse(contractRawData).abi;
 
-const provider = new JsonRpcProvider(process.env.KLAYTN_RPC_URL);
+const provider = new JsonRpcProvider(config.klayRpcUrl);
 
 const MARKET_CONTRACT_ADDRESS = '0x0997d2d5cE4bA4036B396fd1b2cceFbF4BeA8Ec2';
 const contract = new Contract(MARKET_CONTRACT_ADDRESS, abi, provider);
 
 const pool = new Pool({
-  host: 'db.ltfdhwvztsqsskvggemu.supabase.co',
-  database: 'postgres',
-  port: 5432,
-  user: 'postgres',
-  password: process.env.SUPABASE_DB_PW ?? '',
+  host: config.dbHost,
+  database: config.databaseName,
+  port: config.dbPort,
+  user: config.dbUser,
+  password: config.dbPassword,
   max: 5,
 });
 
